6816ec7dc22eb801b7f8a064be0d4df24608f0e6
scurenza <fedechiar@hotmail.it>
2022-10-20 14:53:33 +0200

    milestone 0 e 1 complete

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto. */
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
+
+// Array
+const teamArray = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        pic: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        pic: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        pic: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        pic: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Scott Estrada",
+        pic: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        pic: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+for (let key in teamArray) {
+    console.log(teamArray[key]);
+}
